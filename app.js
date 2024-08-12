4bc1534fa0e12fc28b8e9e4d1a4548a3a752bb93
zohidbe <zohaxaydarov@gmail.com>
2024-08-12 19:23:24 +0500

    end homwork

---
+++ app.js
@@ -0,0 +1,43 @@
+// let arr =  [,2,3,4,5,6,7,8,9,];
+
+//  const x = (number) => {
+//     let newArr = [];
+//     for (let i = 0; i < number.lenght; i++) {
+//         if (number[i] % 2 == 0) {
+//             newArr.push(number[i])
+//         }
+//     } 
+//     return newArr;
+//  };
+
+//  x(arr);
+
+
+
+// const x = (number) => {
+//     let count = 0;
+
+// }
+// function harfSoniSanash(arr) {
+//     let harfObj = {};
+    
+//     for (let i = 0; i < arr.length; i++) {
+//         let harf = arr[i];
+        
+//         if (harf === "a") {
+//             if (harfObj[harf]) {
+//                 harfObj[harf]++;
+//             } else {
+//                 harfObj[harf] = 1;
+//             }
+//         }
+//     }
+    
+//     return harfObj["a"] || 0;
+// }
+
+// let arr = ["a", "b", "a", "12", "32", "a"];
+// let aHarfiSanasi = harfSoniSanash(arr);
+// console.log(aHarfiSanasi); // Natija: 3
+
+
---
+++ homwork.js
@@ -0,0 +1,43 @@
+ // task 1
+
+//  function lastChars(a, b) {
+//     let firstChar = a.charAt(0);
+//     let lastChar = b.length > 0 ? b.charAt(b.length - 1) : "@";
+//     return firstChar + lastChar;
+//   }
+//   console.log(lastChars("last", "chars")); 
+//   console.log(lastChars("yo", "java"));    
+//   console.log(lastChars("hi", ""));        
+  
+
+// task 2
+
+//  function seeColor(str) {
+// if (str.startsWith("red")) {
+//     return "red";
+// } else if (str.startsWith("blue")) {
+//     return "blue";
+// } else { 
+//     return "";
+// } }
+
+// console.log(seeColor("redxx"));
+// console.log(seeColor("xxred"));
+// console.log(seeColor("blueTImes"));
+
+// task 3 
+
+// function middleThree(str) {
+//     const len = str.length;
+//     if (len < 3) {
+//       return ""; 
+//     }
+//     const midIndex = Math.floor(len / 2);
+
+//     return str.substring(midIndex - 1, midIndex + 2);
+//   }
+
+//   console.log(middleThree("Candy"));  // "and"
+//   console.log(middleThree("and"));    // "and"
+//   console.log(middleThree("solving")); // "lvi"
+  
